21a9740e854ee795d303ef0c4ab041ab87c4f20c
MeganSteenkamp <megan.steenkamp@gmail.com>
2021-06-16 14:07:31 +1200

    remove unused imports

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -3,14 +3,12 @@ import {
   Arg,
   Ctx,
   Field,
-  FieldResolver,
   InputType,
   Int,
   Mutation,
   ObjectType,
   Query,
   Resolver,
-  Root,
   UseMiddleware,
 } from 'type-graphql';
 import { MyContext } from '../types';
