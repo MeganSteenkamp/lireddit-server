a2f31a92a34cf0b28cf1a9c1ea00dcd2e567681a
MeganSteenkamp <megan.steenkamp@gmail.com>
2021-07-12 11:45:50 +1200

    fix cookie for deployment

--- src/index.ts
+++ src/index.ts
@@ -42,6 +42,7 @@ const main = async () => {
   const RedisStore = connectRedis(session);
   const redis = new Redis();
 
+  app.set('trust proxy', 1);
   app.use(
     cors({
       origin: 'http://localhost:3000',
